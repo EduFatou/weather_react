58c5646be35c10a8e29bedc5f04b3432c1b85d57
EduFatou <edufatou@gmail.com>
2024-09-02 19:51:05 +0200

    added footer

---
+++ src/components/Main/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return <div>Hecho con
+    <div className="text-red-500 px-2 text-2xl">&#10084;</div>
+    por Edu Fatou</div>;
+  }
+}
+
+export default Footer;
